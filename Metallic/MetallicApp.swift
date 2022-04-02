76ee771de95a4fed8ad27af4f8e13e0fb0b5fec2
Roman Putintsev <rptsev@gmail.com>
2022-04-02 12:31:24 +0300

    Initial Commit

---
+++ Metallic/MetallicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetallicApp.swift
+//  Metallic
+//
+//  Created by Роман Путинцев on 02.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct MetallicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
